97c89a9d24b55fc7a23ae59b80165cc6d8229e04
YasinUnall <yasinunalnn@gmail.com>
2022-06-30 23:58:02 +0300

    Rinkeby test net added to config. Deploy lottery created.

---
+++ scripts/deploy_lottery.py
@@ -0,0 +1,12 @@
+from scripts.helpful_scripts import get_account, get_contract
+from brownie import Lottery
+
+
+def deploy_lottery():
+    account = get_account(id="test_account_1")
+    lottery = Lottery.deploy(get_contract("eth_usd_price_feed").address)
+
+
+def main():
+    deploy_lottery()
+
